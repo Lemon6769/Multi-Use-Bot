0d84529ad5e2ba51bb0b1382bb8072cc3a8844c2
Lemon6769 <jessopvinnie58@gmail.com>
2026-05-16 17:11:48 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -88,8 +88,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
-      secondary: "#2F3136", 
+      primary: "#000080", 
+      secondary: "#32CD32", 
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287", 
